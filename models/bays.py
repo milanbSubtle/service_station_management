5f52c0dbfd48de19616a8b4c7c826908793b7f00
Milan <milanb@subtletechs.com>
2022-04-01 17:09:53 +0530

    [UPDATE]- service_management.py updated (write() and create())

--- models/bays.py
+++ models/bays.py
@@ -9,13 +9,17 @@ class Bay(models.Model):
 
     name = fields.Char(string="Bay Name", required=True)
     code = fields.Char(string="Bay Code")
+    state = fields.Selection([('available', 'Available'), ('reserved', 'Reserved')])
+
+    # avoid duplicate bay codes
     _sql_constraints = [
         ('code_duplicate_error', 'unique(code)', "Bay Code cannot be duplicated !"),
     ]
-    #
+    # avoid duplicate bay codes
     # @api.onchange('code')
     # def _check_bay_code(self):
     #     bay_objects = self.env['bay'].search([])
     #     for bay_object in bay_objects:
     #         if self.code == bay_object.code:
     #             raise UserError(_('Bay Code cannot be duplicated'))
+
--- models/service_management.py
+++ models/service_management.py
@@ -9,16 +9,35 @@ class ServiceManagement(models.Model):
 
     vehicle = fields.Many2one(comodel_name="vehicle", string="Vehicle", required=True)
     customer = fields.Many2one(comodel_name="res.partner", string="Customer")
-    date_time = fields.Datetime(string="Date and Time", default=datetime.now())
+    date_time = fields.Datetime(string="Start Date and Time", default=datetime.now())
     estimated_date_time = fields.Datetime(string="Estimated Date and Time", compute="_compute_estimate_date_time")
     record_lines = fields.One2many(comodel_name="service.record.lines", inverse_name="service_management_id",
                                    string="Record Lines")
     total = fields.Float(string="Total", compute="_calculate_total")
-    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'), ('invoice', 'Invoiced'), ('paid', 'Paid')],
-                             string="States", default="draft")
-    actual_end_date = fields.Datetime(string="Actual Date and Time")
+    state = fields.Selection(
+        [('draft', 'Draft'), ('confirm', 'Confirmed'), ('in_progress', 'In progress'), ('done', 'Done'),
+         ('invoice', 'Invoiced'), ('paid', 'Paid')],
+        string="States", default="draft")
+    actual_end_date = fields.Datetime(string="Actual End Date and Time")
     # sequence number in service management
     sequence_no = fields.Char(string="Service Reference", default=lambda self: _('New'))
+
+    @api.model
+    def create(self, vals):
+        start_date = vals['date_time']
+        end_date = vals['actual_end_date']
+        start_date_datetime = datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+        end_date_datetime = datetime.strptime(end_date, '%Y-%m-%d %H:%M:%S')
+        if start_date_datetime > end_date_datetime:
+            raise UserError(_("Actual End Date and Time and Start Date and Time not matching!!"))
+        return super(ServiceManagement, self).create(vals)
+
+    def write(self, values):
+        if values.get('actual_end_date'):
+            actual_end_datetime = datetime.strptime(values.get('actual_end_date'), '%Y-%m-%d %H:%M:%S')
+            if self.date_time > actual_end_datetime:
+                raise UserError(_("Actual End Date and Time and Start Date and Time not matching!!"))
+        return super(ServiceManagement, self).write(values)
 
     @api.onchange("vehicle")
     def _set_customer(self):
@@ -43,11 +62,28 @@ class ServiceManagement(models.Model):
         sequence = self.env['ir.sequence'].next_by_code('service_sequence') or _('New')
         self.write({'state': 'confirm', 'sequence_no': sequence})
 
+    def action_in_progress(self):
+        service_lines = self.record_lines
+        for service_line in service_lines:
+            if service_line.bay_lines:
+                service_line.bay_lines.write({'state': 'reserved'})
+        self.write({'state': 'in_progress'})
+
+    def action_done(self):
+        service_lines = self.record_lines
+        for service_line in service_lines:
+            if service_line.bay_lines:
+                service_line.bay_lines.write({'state': 'available'})
+        self.write({'state': 'done'})
+
     def action_invoice(self):
         if self.actual_end_date:
             self.write({'state': 'invoice'})
         else:
             raise UserError(_('Please update actual end date'))
+
+    def action_paid(self):
+        self.write({'state': 'paid'})
 
 
 class ServiceRecordLines(models.Model):
@@ -62,7 +98,8 @@ class ServiceRecordLines(models.Model):
     total_price = fields.Monetary(currency_field='res_currency', string="Total Price", readonly=True)
     res_currency = fields.Many2one(comodel_name='res.currency', default=lambda self: self.env.company.currency_id)
     # bay lines in services
-    bay_lines = fields.Many2one(comodel_name="bay", string="Bay Lines")
+    bay_lines = fields.Many2one(comodel_name="bay", string="Bay Lines",
+                                domain="[('state', '=', 'available')]")  # bay line code
 
     @api.onchange('service_type')
     def _onchange_price(self):
