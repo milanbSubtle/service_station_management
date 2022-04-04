73f0a8c90bdf009f3f6fd70e453144f30065b279
Milan <milanb@subtletechs.com>
2022-04-04 17:21:58 +0530

    [ADDED]- vehicle_details_template.xml & vehicle_details_view.xml views added(generate pdf reports)

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Service Station Management System',
-    'version' : '1',
+    'name': 'Service Station Management System',
+    'version': '1',
     'depends': [
         'base',
         'product',
         'account'
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/vehicle_view.xml',
         'views/service_management_view.xml',
         'views/account_move_views.xml',
         'views/bay_view.xml',
+        'views/vehicle_details_view.xml',
         'views/menu.xml',
-        'security/ir.model.access.csv'
+        'report/vehicle_details_template.xml'
     ],
 
 
